b92a5992b4ba45b0594f7e6afc802a327313af19
Chris Cody <3807221+aussiechris@users.noreply.github.com>
2023-01-09 14:58:58 +1100

    create basic game state and logic

--- tutorial/src/main.rs
+++ tutorial/src/main.rs
@@ -1,3 +1,18 @@
+#![allow(dead_code, unused_variables)]
+
+use rusty_engine::prelude::*;
+
+struct GameState {
+    health_left: i32,
+}
+
 fn main() {
-    println!("Hello, world 2!");
+    let mut game = Game::new();
+    let game_state = GameState { health_left: 42 };
+    game.add_logic(game_logic);
+    game.run(game_state);
+}
+
+fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
+    println!("Logical...")
 }
